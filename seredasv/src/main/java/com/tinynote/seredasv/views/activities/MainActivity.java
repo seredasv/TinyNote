1d8b990987fc0cc0e6b7f3ac3ad0414a20a7559d
ssv <sergey.sereda@hotmail.com>
2016-03-06 23:11:07 +0200

    remove navigation drawer, add mikepenz library for drawer

--- seredasv/src/main/java/com/tinynote/seredasv/views/activities/MainActivity.java
+++ seredasv/src/main/java/com/tinynote/seredasv/views/activities/MainActivity.java
@@ -1,17 +1,11 @@
 package com.tinynote.seredasv.views.activities;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
 import com.tinynote.seredasv.R;
 import com.tinynote.seredasv.views.MainApplication;
 import com.tinynote.seredasv.views.fragments.NotesFragment;
@@ -22,15 +16,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String NOTES_TOOLBAR_TITLE = "Notes";
     @ViewById(R.id.container)
     protected FrameLayout container;
-    @ViewById(R.id.drawer_layout)
-    protected DrawerLayout drawerLayout;
     @ViewById(R.id.toolbar)
     protected Toolbar toolbar;
-    @ViewById(R.id.drawer)
-    protected RelativeLayout drawer;
     private Bundle savedInstanceState;
-    private ActionBarDrawerToggle drawerToggle;
-    private float lastTranslate = 0.0f;
 
     @AfterInject
     protected void afterInject() {
@@ -39,57 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     @AfterViews
     protected void afterViews() {
-        createToolbar();
-        createNavigationDrawer();
         openNotesFragment();
-    }
-
-    private void createToolbar() {
-        if (toolbar != null) {
-            toolbar.setNavigationIcon(R.mipmap.ic_menu_white_24dp);
-//            toolbar.setTitle();
-            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    //TODO open drawer here
-                }
-            });
-        }
-    }
-
-    private void createNavigationDrawer() {
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name) {
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                supportInvalidateOptionsMenu();
-            }
-
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
-                super.onDrawerSlide(drawerView, slideOffset);
-                float moveFactor = (drawer.getWidth() * slideOffset);
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-                    container.setTranslationX(moveFactor);
-                } else {
-                    TranslateAnimation anim = new TranslateAnimation(lastTranslate, moveFactor, 0.0f, 0.0f);
-                    anim.setDuration(0);
-                    anim.setFillAfter(true);
-                    container.startAnimation(anim);
-
-                    lastTranslate = moveFactor;
-                }
-            }
-
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                supportInvalidateOptionsMenu();
-            }
-        };
-
-        drawerLayout.setDrawerListener(drawerToggle);
-        drawerToggle.setDrawerIndicatorEnabled(false);
-        drawerToggle.syncState();
     }
 
     @UiThread
