d83770bc4c5825e15157009b2bfd1533f325db9d
ssv <sergey.sereda@hotmail.com>
2016-03-11 23:40:04 +0200

    implement noteFragment, add strings, menu_note and dialog_link

--- seredasv/src/main/java/com/tinynote/seredasv/views/fragments/NoteFragment.java
+++ seredasv/src/main/java/com/tinynote/seredasv/views/fragments/NoteFragment.java
@@ -1,6 +1,271 @@
 package com.tinynote.seredasv.views.fragments;
 
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
+import android.view.*;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import com.tinynote.seredasv.R;
+import com.tinynote.seredasv.views.activities.MainActivity;
+import io.github.mthli.knife.KnifeText;
 
 public class NoteFragment extends Fragment {
+    private static final String TOOLBAR_TITLE = "toolbar_title";
+
+    private static String title;
+    private KnifeText knife;
+    private ImageButton ibBold, ibItalic, ibUnderline, ibStrikeThrough, ibBullet, ibQuote, ibLink, ibClear;
+
+    public static NoteFragment newInstance(String title) {
+        NoteFragment fragment = new NoteFragment();
+
+        Bundle args = new Bundle();
+        args.putString(TOOLBAR_TITLE, title);
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        title = getArguments().getString(TOOLBAR_TITLE);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_note, container, false);
+
+        setHasOptionsMenu(true);
+
+        findView(view);
+
+        ((MainActivity) getActivity()).toolbar.setTitle(title);
+
+        knife.setSelection(knife.getEditableText().length());
+
+        setupBold();
+        setupItalic();
+        setupUnderline();
+        setupStrikethrough();
+        setupBullet();
+        setupQuote();
+        setupLink();
+        setupClear();
+
+        return view;
+    }
+
+    //region setup knife
+    private void setupBold() {
+        ibBold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knife.bold(!knife.contains(KnifeText.FORMAT_BOLD));
+            }
+        });
+
+        ibBold.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(getActivity(), R.string.toast_bold, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupItalic() {
+        ibItalic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knife.italic(!knife.contains(KnifeText.FORMAT_ITALIC));
+            }
+        });
+
+        ibItalic.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(getActivity(), R.string.toast_italic, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupUnderline() {
+        ibUnderline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knife.underline(!knife.contains(KnifeText.FORMAT_UNDERLINED));
+            }
+        });
+
+        ibUnderline.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(getActivity(), R.string.toast_underline, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupStrikethrough() {
+        ibStrikeThrough.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knife.strikethrough(!knife.contains(KnifeText.FORMAT_STRIKETHROUGH));
+            }
+        });
+
+        ibStrikeThrough.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(getActivity(), R.string.toast_strikethrough, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupBullet() {
+        ibBullet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knife.bullet(!knife.contains(KnifeText.FORMAT_BULLET));
+            }
+        });
+
+
+        ibBullet.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(getActivity(), R.string.toast_bullet, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupQuote() {
+        ibQuote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knife.quote(!knife.contains(KnifeText.FORMAT_QUOTE));
+            }
+        });
+
+        ibQuote.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(getActivity(), R.string.toast_quote, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupLink() {
+        ibLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showLinkDialog();
+            }
+        });
+
+        ibLink.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(getActivity(), R.string.toast_insert_link, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupClear() {
+        ibClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knife.clearFormats();
+            }
+        });
+
+        ibClear.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(getActivity(), R.string.toast_format_clear, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+    //endregion
+
+    private void findView(View view) {
+        knife = (KnifeText) view.findViewById(R.id.et_knife);
+        ibBold = (ImageButton) view.findViewById(R.id.bold);
+        ibItalic = (ImageButton) view.findViewById(R.id.italic);
+        ibUnderline = (ImageButton) view.findViewById(R.id.underline);
+        ibStrikeThrough = (ImageButton) view.findViewById(R.id.strikethrough);
+        ibBullet = (ImageButton) view.findViewById(R.id.bullet);
+        ibQuote = (ImageButton) view.findViewById(R.id.quote);
+        ibLink = (ImageButton) view.findViewById(R.id.link);
+        ibClear = (ImageButton) view.findViewById(R.id.clear);
+    }
+
+    private void showLinkDialog() {
+        final int start = knife.getSelectionStart();
+        final int end = knife.getSelectionEnd();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setCancelable(false);
+
+        View view = getLayoutInflater(null).inflate(R.layout.dialog_link, null, false);
+        final EditText editText = (EditText) view.findViewById(R.id.edit_text);
+        builder.setView(view);
+        builder.setTitle(R.string.dialog_title);
+
+        builder.setPositiveButton(R.string.dialog_button_ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String link = editText.getText().toString().trim();
+                if (TextUtils.isEmpty(link)) {
+                    return;
+                }
+
+                // When KnifeText lose focus, use this method
+                knife.link(link, start, end);
+            }
+        });
+
+        builder.setNegativeButton(R.string.dialog_button_cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        builder.create().show();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.undo:
+                knife.undo();
+                break;
+            case R.id.redo:
+                knife.redo();
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
 }
